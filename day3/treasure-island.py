52845166a9bcc00a26ed920be0f52a2d66bd7389
ttrauger <thethetrain@gmail.com>
2024-04-19 12:48:52 -0400

    coming soon (tm)

--- day3/treasure-island.py
+++ day3/treasure-island.py
@@ -27,3 +27,20 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
+first_dir = input("What direction would you like to start?\n")
+if first_dir.lower() != 'left':
+    print("You begin by turning right and walking.\nYou feel the ground disappear beneath your feet and realize that wasn't the 'right' direction\n Game over.")
+else:
+    second_dir = input("You begin a light jaunt to your left. Eventually you approach a trench filled with water. What would you like to do?\n")
+    if second_dir.lower() != 'wait':
+        print("A ferocious school of trout sense your movement and begin soaring towards you at unimaginable speeds. In your last moments you only feel a strange, sudden craving for fish and chips.\nGame over.")
+    else:
+        third_dir = input("As you finish cleaning your navel while waiting around you suddenly catch a glimpse of something shimmering nearby. You approach the shimmer and a red, blue, and yellow door appear. \nWhich door would you like to open?\n")
+        if third_dir.lower() == 'yellow':
+            print('Congratz bro you did it')
+        elif third_dir.lower() == 'red':
+            print('As you turn the knob on the red door a bead of sweat drips down your brow. Oops - you immolated.\n Game over.')
+        elif third_dir.lower() == 'blue':
+            print("You quickly swing open the blue door and rapidly scan the next area for treasure. In fact, you are so focused on the search for treasure you hardly feel the pain in your legs as three vicious wild boars begin devouring you.\n Game over.")
+        else:
+            print("'Wait what are you doing?'\n'That's not going to work.'\nYou feel all of reality slip away as your consciousness ceases to exist.\nAll that remains is a distant, echoing voice:\n'We don't have the budget to consider every goddamn lunatic's random decision! Just do the doors and ship it so we can get paid!'\nGame over.")
