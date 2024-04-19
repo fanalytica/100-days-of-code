d4cfd3d20ebfc172871471024993fd0cd729045c
ttrauger <thethetrain@gmail.com>
2024-04-19 10:15:33 -0400

    task failed successfully

--- day1/main.py
+++ day1/main.py
@@ -1,10 +1,14 @@
 #1. Create a greeting for your program.
+print("Welcome to the band name generator which is totally not a social engineering program to gather potential security question data.")
 
 #2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
 
 #3. Ask the user for the name of a pet.
+pet = input("What is the name of your most beloved pet?\n")
 
 #4. Combine the name of their city and pet and show them their band name.
+print("Your secu- I mean your band name is: " + city + " " + pet)
 
 #5. Make sure the input cursor shows on a new line:
 
