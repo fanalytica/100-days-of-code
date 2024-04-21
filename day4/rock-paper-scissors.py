67dfc3616e467233831ed883b5667fc72e272859
ttrauger <thethetrain@gmail.com>
2024-04-21 11:28:24 -0400

    fin

--- day4/rock-paper-scissors.py
+++ day4/rock-paper-scissors.py
@@ -26,4 +26,35 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random
+pchoice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 
+game = [rock,paper,scissors]
+pchoice = int(pchoice)
+print(game[pchoice])
+
+print("Computer chose:\n")
+cchoice = random.randint(0,2)
+print(game[cchoice])
+lose = "You lose."
+win = "You win."
+draw = "It's a draw."
+
+if pchoice == 0 and cchoice== 1:
+    print(lose)
+elif pchoice == 1 and cchoice == 2:
+    print(lose)
+elif pchoice == 2 and cchoice == 0:
+    print(lose)
+elif pchoice == cchoice:
+    print(draw)
+else:
+    print(win)
+
+#always lose
+# if pchoice == 0:
+#     print(game[1])
+# elif pchoice == 1:
+#     print(game[2])
+# elif pchoice == 2:
+#     print(game[0])
