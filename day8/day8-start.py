397315a4c04ae65b05588c41b4893ba7ab76635c
ttrauger <thethetrain@gmail.com>
2024-04-22 10:29:27 -0400

    day8 start

---
+++ day8/day8-start.py
@@ -0,0 +1,7 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Jerry", "New York")
+
+greet_with(location="New York", name = "Jerry2")
