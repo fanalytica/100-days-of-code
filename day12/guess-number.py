428a97cc7db3de8ac40ec645dfd12cc3f0df250f
ttrauger <thethetrain@gmail.com>
2024-04-23 18:52:25 -0400

    day12 fin

--- day12/guess-number.py
+++ day12/guess-number.py
@@ -7,4 +7,55 @@
 # Track the number of turns remaining.
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+from art import logo
+import random
+print(logo)
 
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+
+answer = random.randint(0,100)
+
+# TODO-2
+# Set attempts based on select difficulty
+game_modes = {
+    'easy': 10,
+    'hard': 5
+}
+
+# TODO-1
+# Ask user for easy or hard version
+
+while True:
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty in game_modes:
+        attempts = game_modes[difficulty]
+        break
+    else:
+        print("Chosen difficulty unavailable. ")
+
+
+# TODO-3
+# Prompt user for guess and loop untill out of attempts or a correct guess
+def user_guess():
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    guess = input("Make a guess: ")
+    return guess
+
+guess = -1
+
+while guess != answer and attempts >0:
+    guess = int(user_guess())
+    attempts -=1
+# TODO-4
+# Tell the user whether they are high or low
+    if guess > answer:
+        print("Too high.\nGuess again.")
+    elif guess < answer:
+        print("Too low.\nGuess again.")
+
+if guess == answer:
+    print("You won!")
+else:
+    print("You lost!")
