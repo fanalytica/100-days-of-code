0a0f20576b471fdc1c3224666b0e1c4e606bb04e
ttrauger <thethetrain@gmail.com>
2024-04-24 18:49:09 -0400

    day15 start

---
+++ day15/main.py
@@ -0,0 +1,57 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+#TODO prompt user input
+# Ask for type of coffee
+# Dispense if enough money
+# repeat
+
+#TODO Function to turn off coffee machine
+
+#TODO Report function to show current available resources
+# Water, milk, coffee, money
+
+#TODO Verify sufficient resources for requested drink
+
+#TODO Request payment
+# Prompt user for payment in quarters, dimes, nickles, pennies
+
+#TODO Process payment
+# Verify sufficent funds provided by user for cost of drink
+# If sufficient add cost to machine profits
+# If not sufficient refund money
+# If too much money return change
+
+#TODO Make coffee
+# If payment is successfully processed and sufficient resources available  make drink
+# Deduct used resources from available resources
+# Tell the user “Here is your latte. Enjoy!”. If latte was their choice of drink
