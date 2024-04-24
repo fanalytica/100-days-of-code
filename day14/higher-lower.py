b769e1b56117677a1c87a9a01d78464b6e03057f
ttrauger <thethetrain@gmail.com>
2024-04-24 09:27:32 -0400

    day14 start

---
+++ day14/higher-lower.py
@@ -0,0 +1,29 @@
+# TODO-0
+# Break down problem
+# TODO-Obj
+# Create a game that shows the user two choices and they have to choose which option has more followers
+
+from game_data import data
+from art import logo, vs
+
+# TODO-1
+# Introduce game
+print(logo)
+
+
+
+# TODO-2 
+# Randomly present two options for the user
+print(f" Compare A: {choice_a}, a {choice_a['description'], from {choice_a['country']}}")
+
+print(vs)
+
+print(f" Compare B: {choice_b}, a {choice_b['description'], from {choice_b['country']}}")
+# TODO-3
+# Prompt the user to guess
+
+# TODO-4
+# Determine if they are corret
+# If incorrect end game
+# If correct continue using the correct guess and compare to another random option
+# Present the new choices to the user, repeat
