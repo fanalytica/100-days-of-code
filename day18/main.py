2546878be4504c23cce321bd11cd2e3f0887fc9e
ttrauger <thethetrain@gmail.com>
2024-04-29 15:49:08 -0400

    lesson finish

---
+++ day18/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+drawing = turtle.Turtle()
+drawing.shape("turtle")
+drawing.color("red")
+
+for _ in range(4):
+    drawing.forward(100)
+    drawing.right(90)
+
+
+
+
+
+screen = turtle.Screen()
+screen.exitonclick()
